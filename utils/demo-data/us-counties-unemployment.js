9ffe82f4a7fca7edffeb40424b2f78072f75ab32
cosycode <32601113+cosycode@users.noreply.github.com>
2018-03-02 14:55:09 +0100

    Moved demo-data into a new folder

--- utils/demo-data/us-counties-unemployment.js
+++ utils/demo-data/us-counties-unemployment.js
@@ -3,8 +3,8 @@ const FS = require('fs');
 const Path = require('path');
 const Request = require('request');
 
-const url = 'https://www.bls.gov/web/metro/laucntycur14.txt';
 const txtParser = new RegExp('(\\w+)\\s\\|\\s+(\\d+)\\s+\\|\\s+(\\d+)\\s+\\|\\s+([\\s\\w]+)\\,\\s(\\w{2,2})\\s+\\|\\s+(\\w\\w\\w)-(\\d\\d)\\s+\\|\\s+([\\d\\,]+)\\s+\\|\\s+([\\d\\,]+)\\s+\\|\\s+([\\d\\,]+)\\s+\\|\\s+([\\d\\.]+)', 'gm');
+const url = 'https://www.bls.gov/web/metro/laucntycur14.txt';
 
 module.exports = function () {
     return new Promise((resolve, reject) => Request.get(url, {},
