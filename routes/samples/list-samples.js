highcharts/highcharts-utils

commit be1958c488f1a35d68c8bb5448773202e57ab4a6

Fix url in getSample after switch to absolute path

routes/samples/list-samples.js
@@ -3,15 +3,21 @@ const express = require('express');
 const router = express.Router();
 const f = require('./../../lib/functions.js');
 const fs = require('fs');
-const { join, resolve } = require('path');
+const { join, relative, resolve, sep } = require('path');
 
 const highchartsDir = resolve(require('./../../config.json').highchartsDir);
 const samplesDir = join(highchartsDir, 'samples');
 
-
+/**
+ * Creates a serializable representation of a sample.
+ *
+ * @param {string} path Absolute file path to sample directory
+ * @returns {object} Return sample
+ */
 const getSample = (path) => {
 	let sample = {
-		path: path,
+		// path is a url relative to sample directory
+		path: relative(samplesDir, path).split(sep).join('/'),
 		details: f.getDetails(path),
 		files: {}
 	};
@@ -24,7 +30,7 @@ const getSample = (path) => {
 		'test-notes.html'
 
 	].forEach(extraFile => {
-		let filePath = join(samplesDir, path, extraFile);
+		let filePath = join(path, extraFile);
 		if (fs.existsSync(filePath)) {
 			sample.files[extraFile] = true;
 		}

commit dfce77f209e6d632281a1af9285c3f68c9bbb7a2

Fix missing demo.details
Regression since be1958c

routes/samples/list-samples.js
@@ -11,13 +11,13 @@ const samplesDir = join(highchartsDir, 'samples');
 /**
  * Creates a serializable representation of a sample.
  *
- * @param {string} path Absolute file path to sample directory
+ * @param {string} path An relative path to sample directory
  * @returns {object} Return sample
  */
 const getSample = (path) => {
 	let sample = {
 		// path is a url relative to sample directory
-		path: relative(samplesDir, path).split(sep).join('/'),
+		path,
 		details: f.getDetails(path),
 		files: {}
 	};
@@ -55,7 +55,9 @@ const getSamples = () => {
 							fs.lstatSync(path).isDirectory() &&
 							fs.existsSync(join(path, 'demo.html'))
 						) {
-							samples.push(getSample(path));
+							const relativePath = relative(samplesDir, path)
+								.split(sep).join('/');
+							samples.push(getSample(relativePath));
 						}
 					});
 				}