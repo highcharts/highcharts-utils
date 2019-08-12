68f7d6ddda4da201f94ac15c92a4d560d32e282d
Torstein Hønsi <torstein@highsoft.com>
2019-08-12 09:10:29 +0200

    Update nightly.js

--- public/javascripts/samples/nightly.js
+++ public/javascripts/samples/nightly.js
@@ -1,12 +1,15 @@
 /* global Highcharts */
+
+const BUCKET = 'https://s3.eu-central-1.amazonaws.com/staging-code.highcharts.com';
+
+
 const compare = (sample, date) => { // eslint-disable-line no-unused-vars
 
     const dateString = Highcharts.dateFormat('%Y-%m-%d', date);
-    const folder = sample.split('/')[2];
     document.getElementById('reference').src = 
-        `https://s3.eu-central-1.amazonaws.com/staging-code.highcharts.com/test/visualtests/reference/latest/${folder}/reference.svg`
+        `${BUCKET}/test/visualtests/reference/latest/${sample}/reference.svg`
     document.getElementById('candidate').src = 
-        `https://s3.eu-central-1.amazonaws.com/staging-code.highcharts.com/test/visualtests/diffs/${dateString}/${folder}/candidate.svg`;
+        `${BUCKET}/test/visualtests/diffs/${dateString}/${sample}/candidate.svg`;
         
         
     let showingCandidate = false;
@@ -18,33 +21,47 @@ const compare = (sample, date) => { // eslint-disable-line no-unused-vars
             '<h4>Showing reference</h4><small>Click image to show candidate</small>';
         
     }
-    toggle();
-    document.getElementById('candidate').addEventListener('click', toggle);
+    
+    let interval = setInterval(toggle, 500);
+
+    document.getElementById('candidate').addEventListener('click', () => {
+        clearInterval(interval);
+        toggle();
+    });
 
 };
 
 (async () => {
     const startDate = Date.UTC(2019, 7, 8);
-    const endDate = Date.UTC(2019, 7, 9);
+    const endDate = Date.UTC(2019, 7, 12);
     const results = {};
     const samples = {};
     for (let date = startDate; date <= endDate; date += 24 * 36e5) {
         const dateString = Highcharts.dateFormat('%Y-%m-%d', date);
-        const result = await fetch(`https://s3.eu-central-1.amazonaws.com/staging-code.highcharts.com/test/visualtests/diffs/${dateString}/visual-test-results.json`);
-        const data = await result.json();
-        results[date] = data;
-        Object.keys(data).forEach(key => {
-            if (key !== 'meta') {
-                samples[key] = true
-            }
-        });
+
+        try {
+            const result = await fetch(
+                `${BUCKET}/test/visualtests/diffs/${dateString}/visual-test-results.json`
+            );
+            const data = await result.json();
+            results[date] = data;
+            Object.keys(data).forEach(key => {
+                if (key !== 'meta') {
+                    samples[key] = true
+                }
+            });
+        } catch (e) {
+            console.warn(`Failed loading ${dateString}`, e);
+        }
     }
     
     // Render header
     let tr = `<tr><th></th>`;
     for (let date = startDate; date <= endDate; date += 24 * 36e5) {
-        const dateString = Highcharts.dateFormat('%Y-%m-%d', date);
-        tr += `<th>${dateString}</th>`; 
+        if (results[date]) {
+            const dateString = Highcharts.dateFormat('%Y-%m-%d', date);
+            tr += `<th>${dateString}</th>`;
+        }
     }
     tr += '</tr>';
 
@@ -57,11 +74,13 @@ const compare = (sample, date) => { // eslint-disable-line no-unused-vars
                 <th>${sample}</th>
         `;
         for (let date = startDate; date <= endDate; date += 24 * 36e5) {
-            let diff = results[date][sample];
-            if (diff > 0) {
-                diff = `<a href="javascript:compare('${sample}', ${date})">${diff}</a>`;
+            if (results[date]) {
+                let diff = results[date][sample];
+                if (diff > 0) {
+                    diff = `<a href="javascript:compare('${sample}', ${date})">${diff}</a>`;
+                }
+                tr += `<td>${diff}</td>`;
             }
-            tr += `<td>${diff}</td>`;   
         }
         tr += '</tr>';
         
