005749f2f27a0e99a60add5c0f2c9122551c5d17
Torstein Hønsi <torstein@highsoft.com>
2019-10-02 12:16:05 +0200

    Opening for ES modules

--- app-code.js
+++ app-code.js
@@ -9,6 +9,11 @@ const { codePort } = require('./lib/arguments.js');
 
 http.createServer(function(req, res) {
 
+	
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Request-Method', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+	res.setHeader('Access-Control-Allow-Headers', '*');
 	let url = req.url.replace(/^\/(gantt|maps|stock)\//g, '/');
 
 	let file = f.getCodeFile(url);
@@ -18,6 +23,9 @@ http.createServer(function(req, res) {
 		return;
 	}
 
+	if (/\.js$/.test(file.success)) {
+		res.setHeader('Content-Type', 'text/javascript');
+	}
 	res.end(fs.readFileSync(file.success));
     
 }).listen(codePort);
