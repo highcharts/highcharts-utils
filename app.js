d7941683c70bfc6ce00456050772af58f2ce6b9d
Torstein Hønsi <torstein@highsoft.com>
2020-01-06 10:24:16 +0100

    Lint

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res) {
+app.use(function(err, req, res, next) { // eslint-disable-line no-unused-vars
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
