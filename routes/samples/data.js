cecdb4f2f1b4ef4ae9c41916605bfd2ebf54dfec
bre1470 <40056287+bre1470@users.noreply.github.com>
2020-08-17 13:44:58 +0200

    Added fix for handling ES6 modules.

--- routes/samples/data.js
+++ routes/samples/data.js
@@ -4,15 +4,20 @@ const fs = require('fs');
 const { samplesDir } = require('../../lib/arguments.js');
 const { join } = require('path');
 
-router.get(/[a-z\/\-\.]+\.(js|json|csv)$/, function(req, res) {
+router.get(/[a-z\/\-\.]+\.(mjs|js|json|csv)$/, function(req, res) {
 	let file = join(samplesDir, 'data', req.path);
 	if (!fs.existsSync(file)) {
 		res.status(404).send(`File not found: ${file}`);
 		return;
     }
-
-    if (/json$/.test(req.path)) {
+    if (/\.js$/.test(req.path)) {
+        res.setHeader('Content-Type', 'text/javascript');
+    }
+    if (/\.json$/.test(req.path)) {
     	res.setHeader('Content-Type', 'application/json');
+    }
+    if (/\/es-modules\/|\.mjs$/.test(req.path)) {
+        res.setHeader('Content-Type', 'application/javascript+module');
     }
     res.setHeader('Cache-Control', 'public, max-age=60');
 	res.sendFile(file);
