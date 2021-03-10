5109a12c9ea4b91d73e2d59418105735898b49cf
Torstein Hønsi <torstein@highsoft.com>
2021-03-10 16:03:20 +0100

    Worked on pull overview

--- public/modules/pulls.js
+++ public/modules/pulls.js
@@ -1,29 +1,43 @@
+/* global timeago */
 import { createOAuthAppAuth } from "https://cdn.skypack.dev/@octokit/auth-oauth-app";
 import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
 import auth from '/pulls/auth';
 
+const per_page = 20;
+
 
 // Render
 const renderPull = pull => {
-    const lastComment = pull.comments.length &&
-        pull.comments[pull.comments.length - 1] || {};
-
-    const lastCommit = pull.commits.length &&
-        pull.commits[pull.commits.length - 1] || {};
-
+    /*
+    const comments = pull.comments.map(
+        c => {
+            const date = c.created_at || c.submitted_at;
+            return `<div><b>@${c.user.login}:</b> ${c.body.substr(0, 80)} ${date}</div>`
+        }
+    ).join('');
+    */
     document.getElementById('pulls').innerHTML += `
-    <div class="pull">
-        <h4>${pull.title}</h4>
-        <div class="comments">
-            ${pull.comments.length} comments,
-            last by ${lastComment.user.login}
+    <li class="pull list-group-item ${pull.className}">
+        <a href="https://github.com/highcharts/highcharts/pull/${pull.number}"
+                target="_blank">
+            ${pull.title}
+        </a>
+        <span class="badge bg-primary rounded-pill"
+            title="${pull.newInteractionsTitle}">
+            ${pull.newInteractions}
+        </span>
+        <div style="width: 100%">
+            <small class="text-muted">
+                #${pull.number} opened
+                <span class="timeago" datetime="${pull.created_at}"></span>
+                by ${pull.user.login}
+                <i class="fa fa-clock-o"></i> updated
+                <span class="timeago" datetime="${pull.updated_at}"></span>
+            </small>
         </div>
-        <div class="commits">
-            ${pull.commits.length} commits,
-            last by ${lastCommit.author.login}
-        </div>
-    </div>
+    </li>
     `;
+    timeago().render(document.querySelectorAll('.timeago'));
 }
 
 (async () => {
@@ -39,17 +53,73 @@ const renderPull = pull => {
         state: 'open',
         sort: 'updated',
         direction: 'desc',
-        per_page: 3
+        per_page
     }).catch(e => console.error(e));
 
-
+    let i = 0;
+    const docTitle = document.title;
     for (let pull of pulls.data) {
-        const comments = await octokit.pulls.listReviewComments({
+
+        let myLastInteraction = 0;
+
+        // Progress bar
+        document.title = docTitle + ' ' +
+            new Array(Math.round(i * 6 / per_page)).fill('█').join('') +
+            new Array(Math.round((per_page - i) * 6 / per_page)).fill('▁').join('') +
+            ' ' + i + '/' + per_page;
+
+
+        const comments = await octokit.issues.listComments({
+            owner: 'highcharts',
+            repo: 'highcharts',
+            issue_number: pull.number
+        }).catch(e => console.error(e));
+
+        const reviews = await octokit.pulls.listReviews({
             owner: 'highcharts',
             repo: 'highcharts',
             pull_number: pull.number
         }).catch(e => console.error(e));
-        pull.comments = comments.data;
+
+        const reviewComments = await octokit.pulls.listReviewComments({
+            owner: 'highcharts',
+            repo: 'highcharts',
+            pull_number: pull.number
+        }).catch(e => console.error(e));
+
+        pull.comments = comments.data
+            .concat(reviews.data)
+            .concat(reviewComments.data);
+        pull.comments.sort((a, b) =>
+            Date.parse(a.created_at || a.submitted_at) -
+            Date.parse(b.created_at || b.submitted_at)
+        );
+
+        if (pull.comments.length) {
+            pull.lastComment = pull.comments[pull.comments.length - 1];
+            pull.myLastComment = pull.comments.slice().reverse()
+                .find(c => c.user.login === 'TorsteinHonsi');
+            if (pull.myLastComment) {
+                myLastInteraction = Math.max(
+                    myLastInteraction,
+                    Date.parse(
+                        pull.myLastComment.created_at ||
+                        pull.myLastComment.submitted_at
+                    )
+                );
+            }
+        }
+
+        /*
+        // Events like assigned, tagged, review_requested
+        const { data: events } = await octokit.issues.listEvents({
+            owner: 'highcharts',
+            repo: 'highcharts',
+            issue_number: pull.number
+        }).catch(e => console.error(e));
+        pull.events = events;
+        */
+
 
         const commits = await octokit.pulls.listCommits({
             owner: 'highcharts',
@@ -58,7 +128,48 @@ const renderPull = pull => {
         }).catch(e => console.error(e));
         pull.commits = commits.data;
 
-        renderPull(pull);
+        if (pull.commits.length) {
+            pull.lastCommit = pull.commits[pull.commits.length - 1];
+            pull.myLastCommit = pull.commits.slice().reverse()
+                .find(c => c.author.login === 'TorsteinHonsi');
+            if (pull.myLastCommit) {
+                myLastInteraction = Math.max(
+                    myLastInteraction,
+                    Date.parse(pull.myLastCommit.commit.author.date)
+                );
+            }
+        }
+
+        // Count new interactions since myLastInteraction
+        let newComments =  (pull.comments || []).filter(
+            c =>
+            Date.parse(c.created_at || c.submitted_at) > myLastInteraction
+        );
+        let newCommits = (pull.commits || []).filter(
+            c => Date.parse(c.commit.author.date) > myLastInteraction
+        );
+
+        pull.newInteractions = newComments.length + newCommits.length;
+
+        pull.newInteractionsTitle =
+            newComments.map(c =>
+                '@' + c.user.login + ': ' + c.body.substr(0, 60)
+            ).join('\n') +
+            '\n' +
+            newCommits.map(c =>
+                '@' + c.author.login + ': ' + c.commit.message
+            ).join('\n');
+
+        console.log(pull.number, pull.title, pull);
+
+        if (!pull.draft && pull.newInteractions > 0) {
+            renderPull(pull);
+        }
+        i++;
+
+        if (i === per_page) {
+            document.title = docTitle;
+        }
     }
 
 
--- routes/pulls.js
+++ routes/pulls.js
@@ -3,7 +3,16 @@ const router = express.Router();
 
 
 router.get('/', async (req, res) => {
-    res.render('pulls/main');
+    res.render('pulls/main', {
+        styles: [
+            'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css',
+            '/stylesheets/vendor/font-awesome-4.7.0/css/font-awesome.css'
+        ],
+        scripts: [
+            'https://cdnjs.cloudflare.com/ajax/libs/timeago.js/2.0.2/timeago.min.js'
+        ],
+        title: 'Pulls'
+    });
 });
 
 router.get('/auth', async (req, res) => {
