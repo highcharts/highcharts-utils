8f946ffbc538344a7b2d2a44bcdd5792a2260ee0
Gøran A. Slettemark <goran.slettemark@highsoft.com>
2022-12-28 10:01:09 +0100

    removed tabs

--- routes/samples/view-source.js
+++ routes/samples/view-source.js
@@ -5,21 +5,21 @@ const path = require('path');
 const { samplesDir } = require('../../lib/arguments.js');
 
 router.get('/', function(req, res) {
-	let html = path.join(
-		samplesDir,
-		req.query.path,
-		'demo.html'
-	);
-	let css = path.join(
-		samplesDir,
-		req.query.path,
-		'demo.css'
-	);
-	let js = path.join(
-		samplesDir,
-		req.query.path,
-		'demo.js'
-	);
+  let html = path.join(
+    samplesDir,
+    req.query.path,
+    'demo.html'
+  );
+  let css = path.join(
+    samplesDir,
+    req.query.path,
+    'demo.css'
+  );
+  let js = path.join(
+    samplesDir,
+    req.query.path,
+    'demo.js'
+  );
 
   let mjs = path.join(
     samplesDir,
@@ -27,21 +27,21 @@ router.get('/', function(req, res) {
     'demo.mjs'
   );
 
-	res.render('samples/view-source', {
-		scripts: [
-			'//cdnjs.cloudflare.com/ajax/libs/codemirror/4.0.3/codemirror.min.js',
-			'//cdnjs.cloudflare.com/ajax/libs/codemirror/4.0.3/mode/javascript/javascript.min.js',
-			'//cdnjs.cloudflare.com/ajax/libs/codemirror/4.0.3/mode/xml/xml.min.js',
-			'//cdnjs.cloudflare.com/ajax/libs/codemirror/4.0.3/mode/htmlmixed/htmlmixed.min.js',
-			'//cdnjs.cloudflare.com/ajax/libs/codemirror/4.0.3/mode/css/css.min.js'
-		],
-		styles: [
-			'//cdnjs.cloudflare.com/ajax/libs/codemirror/4.0.3/codemirror.min.css'
-		],
-		html: fs.existsSync(html) && fs.readFileSync(html),
-		css: fs.existsSync(css) && fs.readFileSync(css),
-    js: fs.existsSync(js) ? fs.readFileSync(js) : (fs.existsSync(mjs) && fs.readFileSync(mjs)) 
-	});
+  res.render('samples/view-source', {
+    scripts: [
+      '//cdnjs.cloudflare.com/ajax/libs/codemirror/4.0.3/codemirror.min.js',
+      '//cdnjs.cloudflare.com/ajax/libs/codemirror/4.0.3/mode/javascript/javascript.min.js',
+      '//cdnjs.cloudflare.com/ajax/libs/codemirror/4.0.3/mode/xml/xml.min.js',
+      '//cdnjs.cloudflare.com/ajax/libs/codemirror/4.0.3/mode/htmlmixed/htmlmixed.min.js',
+      '//cdnjs.cloudflare.com/ajax/libs/codemirror/4.0.3/mode/css/css.min.js'
+    ],
+    styles: [
+      '//cdnjs.cloudflare.com/ajax/libs/codemirror/4.0.3/codemirror.min.css'
+    ],
+    html: fs.existsSync(html) && fs.readFileSync(html),
+    css: fs.existsSync(css) && fs.readFileSync(css),
+    js: fs.existsSync(js) ? fs.readFileSync(js) : (fs.existsSync(mjs) && fs.readFileSync(mjs))
+  });
 });
 
 module.exports = router;
