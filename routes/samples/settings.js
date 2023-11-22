2cbf4d48e379e35c8ddf6aebb443dd96ddf19998
Gøran A. Slettemark <goran.slettemark@highsoft.com>
2023-11-22 17:02:21 +0100

    Added quick compile on demand setting

--- routes/samples/settings.js
+++ routes/samples/settings.js
@@ -1,37 +1,11 @@
 const express = require('express');
-const fs = require('fs').promises;
 const router = express.Router();
-const help = require('../../lib/settings-help');
-const config = require('../../config.json');
-const { join } = require('path');
 
+const { getConfig } = require('../../lib/functions.js');
 
 router.get('/', async (req, res) => {
+    const options = await getConfig();
 
-	const json = await fs.readFile(
-		join(__dirname, '../..', 'temp', 'config-user.json'),
-		'utf-8'
-	);
-	const user = json ? JSON.parse(json) : {};
-
-	const options = Object.keys(help).map(key => {
-
-		const type = {
-				string: 'text',
-				boolean: 'checkbox',
-				number: 'number'
-			}[typeof config[key]],
-			value = user[key] ?? config[key];
-
-		return {
-			key,
-			help: help[key],
-			type,
-			value,
-			isCheckbox: type === 'checkbox',
-			checked: value ? 'checked' : ''
-		}
-	});
 	res.render('samples/settings', {
 		preJS: req.session.preJS,
 		options
