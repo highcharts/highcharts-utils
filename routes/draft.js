75f84c7abda1b69ca9d3a5259374fe181107c6d3
Torstein Hønsi <torstein@highsoft.com>
2023-11-23 09:21:24 +0100

    Quick settings dropdown

--- routes/draft.js
+++ routes/draft.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const { join } = require('path');
 
 router.get('/', function(req, res) {
-	
+
 	let path = fs.readFileSync(join(__dirname, '../path.txt'), 'utf-8');
 
 	res.redirect(`/samples/view?path=${path}&mobile=true`);
