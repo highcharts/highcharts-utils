6335e0f8c74b8b10d10131a0377f4218f9ccc240
Torstein Hønsi <torstein@highsoft.com>
2024-05-08 13:24:42 +0200

    Friendly warning on file type

--- routes/samples/data.js
+++ routes/samples/data.js
@@ -1,26 +1,42 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
+const path = require('path');
 const { samplesDir } = require('../../lib/arguments.js');
 const { join } = require('path');
 
-router.get(/[a-z\/\-\.0-9]+\.(gpx|mjs|js|json|csv|ttf|xml)$/, function(req, res) {
-	let file = join(samplesDir, 'data', req.path);
-    if (!fs.existsSync(file)) {
-		res.status(404).send(`File not found: ${file}`);
-		return;
+router.get(/[a-z\/\-\.0-9]+\.([a-z]+)$/, function(req, res) {
+
+    const extname = path.extname(req.path).replace(/^./, '');
+
+    let file = join(samplesDir, 'data', req.path);
+
+    if (!['gpx', 'mjs', 'js', 'json', 'csv', 'ttf', 'xml'].includes(extname)) {
+        res.status(403).send(`
+            File type not allowed in the utils: ${extname}.<br>
+            Update the allow-list in <code>routes/samples/data.js</code>.
+        `);
+        return;
     }
-    if (/\.js$/.test(req.path)) {
+
+    if (!fs.existsSync(file)) {
+        res.status(404).send(`File not found: ${file}`);
+        return;
+    }
+    if (extname === 'js') {
         res.setHeader('Content-Type', 'text/javascript');
     }
-    if (/\.json$/.test(req.path)) {
-    	res.setHeader('Content-Type', 'application/json');
+    if (extname === 'json') {
+        res.setHeader('Content-Type', 'application/json');
+    }
+    if (extname === 'xml' || extname === 'gpx') {
+        res.setHeader('Content-Type', 'application/xml');
     }
     if (/\/es-modules\/|\.mjs$/.test(req.path)) {
         res.setHeader('Content-Type', 'application/javascript+module');
     }
     res.setHeader('Cache-Control', 'public, max-age=60');
-	res.sendFile(file);
+    res.sendFile(file);
 });
 
 module.exports = router;
