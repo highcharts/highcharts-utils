353dac2592acb3feb11b2f1218f6ca55da1633e7
Gøran A. Slettemark <goran.slettemark@highsoft.com>
2024-08-26 09:47:26 +0200

    Don't actually run utils

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,7 +71,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'node ./server.js',
+    command: 'npx serve ./public',
     url: 'http://127.0.0.1:3030',
     reuseExistingServer: !process.env.CI,
   },
